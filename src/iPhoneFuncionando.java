fd0650c8d71bf03ae3a2da7412ee51eb98a898f4
Silvia Hardt <silvinha126@gmail.com>
2023-10-13 23:07:08 -0300

    Initial commit

---
+++ src/iPhoneFuncionando.java
@@ -0,0 +1,21 @@
+import Aparelho.iPhone;
+
+public class iPhoneFuncionando {
+    public static void main(String[] args) {
+        iPhone iphone = new iPhone();
+
+        iphone.tocar();
+        iphone.pausar();
+        iphone.selecionarMusica();
+
+        iphone.ligar();
+        iphone.atender();
+        iphone.iniciarCorreioVoz();
+
+        iphone.exibirPagina();
+        iphone.adicionarNovaAba();
+        iphone.atualizarPagina();
+
+    }
+
+}
